e74cb248d71dd1f1a9a0e0b625a4afeed84c94ac
djesecoronel <101542051+djesecoronel@users.noreply.github.com>
2022-03-14 00:05:30 -0500

    Update Punto3.java

--- Punto3.java
+++ Punto3.java
@@ -1,15 +1,5 @@
 package taller.pkg2;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author coronel
- */
 public class Punto3 {
     public static void main (String[] args) {
         
