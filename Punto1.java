050585412d0fd15bfb09acbb79ca700ae7bfc2e6
djesecoronel <101542051+djesecoronel@users.noreply.github.com>
2022-03-14 00:04:30 -0500

    Update Punto1.java

--- Punto1.java
+++ Punto1.java
@@ -1,15 +1,5 @@
 package taller.pkg2;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author asus
- */
 
 import java.util.Scanner;
 
