cd4e6458c8018a6c0c3d48bc8f8089901880aec8
djesecoronel <101542051+djesecoronel@users.noreply.github.com>
2022-03-14 00:05:57 -0500

    Update Punto4.java

--- Punto4.java
+++ Punto4.java
@@ -3,16 +3,6 @@ package taller.pkg2;
 
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author coronel
- */
 public class Punto4 {
     public static void main (String[] args) {
         Scanner entrada = new Scanner (System.in);
